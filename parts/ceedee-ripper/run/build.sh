c4ec31b8045146265b814773e9b5d3323f4d49f7
odioski <o.daniels.sr@outlook.com>
2026-04-23 15:40:10 -0500

    ...

---
+++ parts/ceedee-ripper/run/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -euo pipefail
+source /home/mrod/CeeDeeRipper/parts/ceedee-ripper/run/environment.sh
+set -x
+if cargo read-manifest --manifest-path "."/Cargo.toml > /dev/null; then
+    cargo install -f --locked --path "." --root "/home/mrod/CeeDeeRipper/parts/ceedee-ripper/install" 
+    # remove the installation metadata
+    rm -f "/home/mrod/CeeDeeRipper/parts/ceedee-ripper/install"/.crates{.toml,2.json}
+else
+    # virtual workspace is a bit tricky,
+    # we need to build the whole workspace and then copy the binaries ourselves
+    pushd "."
+    cargo build --workspace --release 
+    # install the final binaries
+    find ./target/release -maxdepth 1 -executable -exec install -Dvm755 {} "/home/mrod/CeeDeeRipper/parts/ceedee-ripper/install" ';'
+    # remove proc_macro objects
+    for i in "/home/mrod/CeeDeeRipper/parts/ceedee-ripper/install"/*.so; do
+        readelf --wide --dyn-syms "$i" | grep -q '__rustc_proc_macro_decls_[0-9a-f]*__' &&                         rm -fv "$i"
+    done
+    popd
+fi                
