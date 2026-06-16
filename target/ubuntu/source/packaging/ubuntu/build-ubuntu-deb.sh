43f328b529ac0557d05ac30cfb8e65d97836cca5
odioski <o.daniels.sr@outlook.com>
2026-06-16 17:14:50 -0500

    ...

--- target/ubuntu/source/packaging/ubuntu/build-ubuntu-deb.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -eu
-
-ROOT="$(CDPATH= cd -- "$(dirname -- "$0")/../.." && pwd)"
-BUILD_ROOT="$ROOT/target/ubuntu/source" \
-PACKAGE_CONTEXT="Ubuntu" \
-exec "$ROOT/packaging/debian/build-debian-deb.sh" "$@"
